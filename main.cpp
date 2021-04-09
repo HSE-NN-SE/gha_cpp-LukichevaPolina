0808f57b73f4335289c43e63347f99a666b0366d
Lukicheva Polina <63358667+LukichevaPolina@users.noreply.github.com>
2021-04-09 13:36:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 //Copyright 2020 Lukicheva Polina
 #include <math.h>
+
+
+#include <cmath>
 #include <stdio.h>
-#include "task1.h"
+
+double delta = 0.00000001;
+
+double average(double x, double y) {
+	return (x + y) / 2.0;
+}
+bool good(double guess, double x) {
+	return fabs(guess * guess - x) < delta;
+}
+double improve(double guess, double x) {
+	return average(guess, x / guess);
+}
+double iter(double guess, double x) {
+	if (good(guess, x))
+		return guess;
+	else
+		return iter(improve(guess, x), x);
+}
+double calc(double arg) {
+	return iter(1.0, arg);
+}
 int main() {
 	double result = calc(0.99999);
 	printf("Sqrt(%lf)=%lf\n", 2.0, result);
