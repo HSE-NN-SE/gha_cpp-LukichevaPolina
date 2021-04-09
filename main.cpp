67f886d2b59e8c09913cce64927a531ef579baad
Lukicheva Polina <63358667+LukichevaPolina@users.noreply.github.com>
2021-04-09 13:22:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main() {
 	Fraction a(1, 0); // 1/2
 	Fraction b(a);
 	Fraction c(5, 6);
-	print("%d", c.getValue());
-	print("%d"c.getValue());
-	print("%d"(a + c).getValue());
+	printf("%d", c.getValue());
+	printf("%d"c.getValue());
+	printf("%d"(a + c).getValue());
 	return 0;
 }
